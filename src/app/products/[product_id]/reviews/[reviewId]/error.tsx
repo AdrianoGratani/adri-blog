4187da6e409767db588ea089c1a2e9b5b8e7ce96
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 14:29:03 +0900

    Update error.tsx with informations

--- src/app/products/[product_id]/reviews/[reviewId]/error.tsx
+++ src/app/products/[product_id]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,7 @@
 //error .tsx is a client component
 "use client"
 
+// when error in ./review/id occurs, automatically wrap the whole segment and its nested children in a React ERror Boundary
 export default function ErrorBoundary(
   {error}: { error: Error }
 ) {
