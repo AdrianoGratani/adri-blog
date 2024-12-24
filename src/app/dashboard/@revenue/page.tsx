ea51aab0a4a02275b586067f09eeca5d46dba7fd
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 18:26:17 +0900

    Update page.tsx   typo

--- src/app/dashboard/@revenue/page.tsx
+++ src/app/dashboard/@revenue/page.tsx
@@ -8,7 +8,7 @@ export default function Revenue() {
     <>
       <ParallelCard>
         Revenue parallel route 
-        <Link href="/dashboard/archived">
+        <Link href="/dashboard/revenue/archived">
         <<<<archive>>>>>
         </Link>
         
