eab05d8914509a6149855d121d9ce71f50ea5887
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 14:44:32 +0900

    Update error.tsx with reset prop for error handling

--- src/app/products/[product_id]/reviews/[reviewId]/error.tsx
+++ src/app/products/[product_id]/reviews/[reviewId]/error.tsx
@@ -3,12 +3,15 @@
 
 // when error in ./review/id occurs, automatically wrap the whole segment and its nested children in a React ERror Boundary
 export default function ErrorBoundary(
-  {error}: { error: Error }
+  {error, reset}: { error: Error, reset: ()=> void }
 ) {
   return (
     <>
       This si Error page for products/id/review/id...
+      
       {error}
+
+      <button onClick={reset}>Try again maybe is not serious. in that case you can use the reset prop for error boundaries</button>
     </>
   )
 }
