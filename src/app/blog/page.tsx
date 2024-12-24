fed72b8986ec98c29acca2f7ae3450fc014669f4
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 14:02:09 +0900

    Update page.tsx with absolute metadata obj

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,13 @@
+import {Metadata} from 'next';
+
+// for this metadata we use object instead of string through next/Metadata;
+// it takes three types key: default template and absolute.
+// export const metadata: Metadata = {
+    // title: {
+//         absolute: blog
+//     }
+// }
+
 export default function Blog() {
 
 
