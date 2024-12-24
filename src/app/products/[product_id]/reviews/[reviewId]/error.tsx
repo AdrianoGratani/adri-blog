fe8e4a5e5634f2631b48e87efc944d4ea999c619
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 14:25:34 +0900

    Create error.tsx for reviews page

---
+++ src/app/products/[product_id]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,10 @@
+//error .tsx is a client component
+"use client"
+
+export default function ErrorBoundary() {
+  return (
+    <>
+      This si Error page for products/id/review/id...
+    </>
+  )
+}
