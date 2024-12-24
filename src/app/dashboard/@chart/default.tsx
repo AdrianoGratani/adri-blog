9b2024a46cee510dc24fd09a04d7386f48048fa8
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 18:46:14 +0900

    Create default.tsx

---
+++ src/app/dashboard/@chart/default.tsx
@@ -0,0 +1,12 @@
+import ParallelCard from "@/components/ParallelCard";
+
+export default function Chart() {
+
+  return (
+    <>
+      <ParallelCard>
+        Chart: parallel page (default: you are in nested url path, this is fallback UI to prevent errors on refresh)
+      </ParallelCard>
+    </>
+  )
+}
