fb50f67eb982a7d5e9f19710653ab0d954034392
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-24 13:59:01 +0900

    Create loading.tsx

---
+++ src/app/blog/second/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+
+  return (
+    <>
+      The page is Loading! this file is Loading version for second/page.tsx to reassure users about the responsiveness of the webapp.
+    </>
+  )
+
+}
