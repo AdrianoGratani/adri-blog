0e6803e96ec63a8fc61dc1c4c438ecd181e0c9ab
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-26 20:20:53 +0900

    Update page.tsx for f1 intercepted

--- src/app/f1/f2/page.tsx
+++ src/app/f1/f2/page.tsx
@@ -5,7 +5,7 @@ export default function F2() {
   return (
     <div>
       This is the page for folder two inside the folder one
-      <Link href="">Click this link to go back to folder one throu intercepted route</Link>
+      <Link href="../f1">Click this link to go back to folder one throu intercepted route</Link>
     </div>
   )
 }
