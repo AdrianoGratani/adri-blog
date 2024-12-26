f402cd8ffb16b1b11bbb211d9e0f4116805d0128
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-26 20:16:40 +0900

    Create page.tsx folder level two for intercepting routes

---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function F2() {
+
+  return (
+    <div>
+      This is the page for folder two inside the folder one
+      <Link href="">Click this link to go back to folder one throu intercepted route</Link>
+    </div>
+  )
+}
