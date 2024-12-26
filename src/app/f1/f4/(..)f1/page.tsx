28f209844a6a3ce7869619b7e003a96db5282313
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-26 20:27:03 +0900

    Create page.tsx intercepted folder 1

---
+++ src/app/f1/f4/(..)f1/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function InterceptedF1(){
+
+  return (
+    <div>
+      this is the intercepted version for folder one, from folder 4
+    <br /> reload to see the page
+    </div>
+  )
+}
