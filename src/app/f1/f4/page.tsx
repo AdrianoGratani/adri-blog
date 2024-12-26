6e2b73529ef924411263afefc0476c7735928a01
Adriano Gratani <adriano.gratani2@gmail.com>
2024-12-26 20:25:22 +0900

    Create page.tsx to intercepte from f4 to one level before

---
+++ src/app/f1/f4/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next-link";
+
+
+export default function F4() {
+
+  return (
+    <div>
+      this is folder four 
+      <Link href="/f1/f3">Click to go to intercepted route 3</Link>
+    </div>
+  )
+}
